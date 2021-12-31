10e6a769228f5b9e26d023a35e2b88a91cba932d
mosesbalaji <moses.balaji-external@tcs.com>
2021-12-31 16:02:18 +0800

    release cnge

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ function getFrom(from, username) {
 }
 
 
+await io.mkdirP(outputDir);
+saveIntermediateData (outputDir, email_list);
+
+function saveIntermediateData (directory, data) {
+  try {
+    const file = path.join(directory, 'email_list.json');
+    fs.writeFileSync (file, JSON.stringify (data));
+  } catch (err) {
+    console.error (Failed to save intermediate data: $ {err});
+  }
+}
+
 
 
 
